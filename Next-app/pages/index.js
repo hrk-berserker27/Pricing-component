eec82252e025802afd4c108355d57ff8776eb533
Hritick <hritickbhushan@gmail.com>
2022-08-08 22:14:46 +0530

    Removed unnecessary details

--- Next-app/pages/index.js
+++ Next-app/pages/index.js
@@ -30,7 +30,13 @@ export default function App() {
               <span>&nbsp;/&nbsp;month</span>
             </span>
           </header>
-          <div className="slider"></div>
+          <div className="slider">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="13">
+              <g fill="#80FFF3" fillRule="evenodd">
+                <path d="M16 2.558v7.884a1 1 0 001.735.679l3.639-3.943a1 1 0 000-1.356l-3.64-3.943A1 1 0 0016 2.558zM6 2.558v7.884a1 1 0 01-1.735.679L.626 7.178a1 1 0 010-1.356l3.64-3.943A1 1 0 016 2.558z" />
+              </g>
+            </svg>
+          </div>
           <div className="toggle-container">
             Monthly Billing <div className="slider"></div>Yearly Billing
             <span>25% discount</span>
